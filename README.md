e28791a2cee7b235df12a174e2a0ab4190be0207
Gauke Pieter Sietzema <gp@sterc.nl>
2019-02-22 10:51:53 +0100

    Updated lexicons en readme

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # MODX Reviews
-![Reviews version](https://img.shields.io/badge/version-1.0.0-red.svg) ![MODX Extra by Sterc](https://img.shields.io/badge/checked%20by-Gauke-blue.svg) ![MODX version requirements](https://img.shields.io/badge/modx%20version%20requirement-2.4%2B-brightgreen.svg)
+![Reviews version](https://img.shields.io/badge/version-1.0.0-brightgreen.svg) ![MODX Extra by Sterc](https://img.shields.io/badge/extra%20by-sterc-magenta.svg) ![MODX version requirements](https://img.shields.io/badge/modx%20version%20requirement-2.4%2B-blue.svg)
 
-## Snippets
+Install the Reviews extra to have central review management. Each review contains the following fields: name, email, what Resource is being reviewed, rating 0-5, review content, active true/false. The extra becomes available in the 'Extras' menu after installing and comes with a Reviews-snippet to easily show one or more reviews on a page and has default-chunks out of the box, which you can of course change if you wish. 
 
-**Voorbeeld snippet call:**
+## Example snippet call
 
 ```
 {'!Reviews' | snippet : [
@@ -14,17 +14,17 @@
 ]}
 ```
 
-**Beschikbare parameters:**
+## Parameters
 
-| Parameter                  | Omschrijving                                                                 |
+| Parameter                  | Description                                                                 |
 |----------------------------|------------------------------------------------------------------------------|
-| id | De ID van de pagina waar de reviews van getoond moeten worden. Standaard de huidige pagina ID. |
-| reviews | De ID's van de reviews die getoond moeten worden, meerdere ID's scheiden met een komma. Deze parameter overschrijft de `id` parameter. |
-| sort | De database kolom waarop gesorteerd word. Standaard `createdon`. |
-| sortDir | De ASC/DESC volgorde waarop gesorteerd word. Standaard `DESC`. |
-| limit | Het aantal reviews die getoond mogen worden. Standaard `0`, indien `0` dan worden alle reviews getoond. |
-| tpl | De template van een review item. Dit kan een chunknaam, `@FILE` of `@INLINE` zijn. |
-| tplWrapper | De tpl van de review items. Dit kan een chunknaam, `@FILE` of `@INLINE` zijn. |
-| tplEmpty | De tpl indien er geen review items zijn. Dit kan een chunknaam, `@FILE` of `@INLINE` zijn. |
-| usePdoTools | Indien `true` dan word pdoTools gebruikt voor de tpl's en is fenom mogelijk (ook `@FILE` en `@INLINE` zijn mogelijk zonder pdoTools). Standaard `false`. |
-| usePdoElementsPath | Indien `true` dan worden `@FILE` tpl's gebruikt vanuit de map die in de `pdotools_elements_path` systeem instelling ingesteld is. Anders word het de `core/components/reviews/` map gebruikt. Standaard `false`. |
+| id | [optional] The Resource ID of the page which is being reviewed (usually a product or service). Default: ID of the current Resource |
+| reviews | [optional] Comma-seperated list of Review IDs to show. This overwrite the ID-parameter. |
+| sort | [optional] The field on which to sort. Default: `createdon`. |
+| sortDir | [optional] Sorting order: ASC/DESC. Default: `DESC`. |
+| limit | [optional] Limit the amount of reviews to show. Default: 0 (unlimited) |
+| tpl | [optional] The review item template. This can be a chunk , `@FILE` or `@INLINE` |
+| tplWrapper | [optional] The review item wrapper-template. This can be a chunk , `@FILE` or `@INLINE` |
+| tplEmpty | [optional] This tpl is shown when no results are found. This can be a chunk , `@FILE` or `@INLINE` |
+| usePdoTools | [optional] Set to `true` to use pdoTools in the tpl's and enable fenom. (`@FILE` and `@INLINE` do not require this). Default: `false` |
+| usePdoElementsPath | [optional] Set to `true` to use the system setting `pdotools_elements_path` as a base-path for the @FILE includes. If `false`, it uses `core/components/reviews/`. Default: `false` |
--- core/components/reviews/lexicon/en/default.inc.php
+++ core/components/reviews/lexicon/en/default.inc.php
@@ -7,10 +7,10 @@
  */
 
 $_lang['reviews']                                       = 'Reviews';
-$_lang['reviews.desc']                                  = 'Manage all reviews.';
+$_lang['reviews.desc']                                  = 'Manage all your reviews. Reviews can be added to any page you want.';
 
 $_lang['area_reviews']                                  = 'Reviews';
-$_lang['area_reviews_editor']                           = 'Chatbots (rich text editor)';
+$_lang['area_reviews_editor']                           = 'Reviews (rich text editor)';
 
 $_lang['setting_reviews.branding_url']                  = 'Branding';
 $_lang['setting_reviews.branding_url_desc']             = 'The URL of the branding button, if the URL is empty the branding button won\'t be shown.';
@@ -18,7 +18,7 @@ $_lang['setting_reviews.branding_url_help']             = 'Branding (help)';
 $_lang['setting_reviews.branding_url_help_desc']        = 'The URL of the branding help button, if the URL is empty the branding help button won\'t be shown.';
 $_lang['setting_reviews.ratings']                       = 'Ratings';
 $_lang['setting_reviews.ratings_desc']                  = 'The min and max rating that can be given. Default is "1||5".';
-$_lang['setting_reviews.use_editor']                    = 'Use rich text editor';
+$_lang['setting_reviews.use_editor']                    = 'Use Rich Text Editor';
 $_lang['setting_reviews.use_editor_desc']               = 'Use a rich text editor for the reviews.';
 $_lang['setting_reviews.editor_plugins']                = 'Plugins';
 $_lang['setting_reviews.editor_plugins_desc']           = 'The \'plugins\' config for the rich text editor.';
@@ -35,22 +35,22 @@ $_lang['setting_reviews.editor_statusbar_desc']         = 'The \'statusbar\' con
 
 $_lang['reviews.review']                                = 'Review';
 $_lang['reviews.reviews']                               = 'Reviews';
-$_lang['reviews.reviews_desc']                          = 'Mange all reviews.';
-$_lang['reviews.review_create']                         = 'Create review';
-$_lang['reviews.review_update']                         = 'Update review';
-$_lang['reviews.review_remove']                         = 'Remove review';
-$_lang['reviews.review_remove_confirm']                 = 'Are you sure you want to remove this review? Because of this, he is no longer included in the statistics.';
-$_lang['reviews.review_activate']                       = 'Activate review';
-$_lang['reviews.review_activate_confirm']               = 'Are you sure you want to activate this review? Because of this, he is included again in the statistics.';
-$_lang['reviews.review_deactivate']                     = 'Deactivate review';
-$_lang['reviews.review_deactivate_confirm']             = 'Are you sure you want to deactivate this review? Because of this, he is no longer included in the statistics.';
+$_lang['reviews.reviews_desc']                          = 'Manage all reviews.';
+$_lang['reviews.review_create']                         = 'Create';
+$_lang['reviews.review_update']                         = 'Update';
+$_lang['reviews.review_remove']                         = 'Remove';
+$_lang['reviews.review_remove_confirm']                 = 'Are you sure you want to remove this review? It will no longer be included in the statistics.';
+$_lang['reviews.review_activate']                       = 'Activate';
+$_lang['reviews.review_activate_confirm']               = 'Are you sure you want to activate this review? It will no longer be included in the statistics.';
+$_lang['reviews.review_deactivate']                     = 'Deactivate';
+$_lang['reviews.review_deactivate_confirm']             = 'Are you sure you want to deactivate this review? It will no longer be included in the statistics.';
 
 $_lang['reviews.label_resource']                        = 'Resource';
 $_lang['reviews.label_resource_desc']                   = 'The resource of the review.';
 $_lang['reviews.label_name']                            = 'Name';
 $_lang['reviews.label_name_desc']                       = 'The name of the reviewer.';
 $_lang['reviews.label_email']                           = 'Email address';
-$_lang['reviews.label_email_desc']                      = 'The email adress of the reviewer.';
+$_lang['reviews.label_email_desc']                      = 'The email address of the reviewer.';
 $_lang['reviews.label_rating']                          = 'Rating';
 $_lang['reviews.label_rating_desc']                     = 'The rating of the review.';
 $_lang['reviews.label_content']                         = 'Review';
@@ -67,4 +67,4 @@ $_lang['reviews.rating_4']                              = 'stars';
 $_lang['reviews.rating_5']                              = 'stars';
 $_lang['reviews.title']                                 = 'Review ([[+total]] total)';
 $_lang['reviews.review_title']                          = 'Review [[+idx]] of [[+total]] by';
-$_lang['reviews.no_reviews']                            = 'There no reviews available for this resource.';
+$_lang['reviews.no_reviews']                            = 'There are no reviews available for this resource.';
