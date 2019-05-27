fd8025ca82ce338c281053f1c66e8d6d1e6c3202
Ivan Bochkarev <ivanx86@gmail.com>
2019-05-27 21:28:26 +0600

    Added Russian translation

---
+++ core/components/reviews/lexicon/ru/default.inc.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Reviews
+ *
+ * Copyright 2018 by Oene Tjeerd de Bruin <oenetjeerd@sterc.nl>
+ */
+
+$_lang['reviews']                                       = 'Отзывы';
+$_lang['reviews.desc']                                  = 'Управление вашими отзывами.';
+
+$_lang['area_reviews']                                  = 'Отзывы';
+$_lang['area_reviews_editor']                           = 'Отзывы (rich text editor)';
+
+$_lang['setting_reviews.branding_url']                  = 'Брендинг';
+$_lang['setting_reviews.branding_url_desc']             = 'URL кнопки брендинга, если URL пуст, кнопка брендинга не будет отображаться.';
+$_lang['setting_reviews.branding_url_help']             = 'Брендинг (помощь)';
+$_lang['setting_reviews.branding_url_help_desc']        = 'URL кнопки справки по брендингу, если URL пуст, кнопка справки по брендингу не будет отображаться.';
+$_lang['setting_reviews.ratings']                       = 'Рейтинги';
+$_lang['setting_reviews.ratings_desc']                  = 'Минимальная и максимальная оценка, которая может быть дана. По умолчанию "1||5".';
+$_lang['setting_reviews.use_editor']                    = 'Использование Rich Text Editor';
+$_lang['setting_reviews.use_editor_desc']               = 'Используйте текстовый редактор для отзывов.';
+$_lang['setting_reviews.editor_plugins']                = 'Плагинов';
+$_lang['setting_reviews.editor_plugins_desc']           = '\'plugins\' конфиг для текстового редактора.';
+$_lang['setting_reviews.editor_toolbar1']               = 'Панель инструментов 1';
+$_lang['setting_reviews.editor_toolbar1_desc']          = '\'toolbar1\' конфиг для текстового редактора.';
+$_lang['setting_reviews.editor_toolbar2']               = 'Панель инструментов 2';
+$_lang['setting_reviews.editor_toolbar2_desc']          = '\'toolbar2\' конфиг для текстового редактора.';
+$_lang['setting_reviews.editor_toolbar3']               = 'Панель инструментов 3';
+$_lang['setting_reviews.editor_toolbar3_desc']          = '\'toolbar3\' конфиг для текстового редактора.';
+$_lang['setting_reviews.editor_menubar']                = 'Строка меню';
+$_lang['setting_reviews.editor_menubar_desc']           = '\'menubar\' конфиг для текстового редактора.';
+$_lang['setting_reviews.editor_statusbar']              = 'Статус бар';
+$_lang['setting_reviews.editor_statusbar_desc']         = '\'statusbar\' конфиг для текстового редактора.';
+
+$_lang['reviews.review']                                = 'Отзыв';
+$_lang['reviews.reviews']                               = 'Отзывы';
+$_lang['reviews.reviews_desc']                          = 'Управляйте всеми вашими отзывами. Отзывы могут быть добавлены на любую страницу, которую вы хотите.';
+$_lang['reviews.review_create']                         = 'Создать';
+$_lang['reviews.review_update']                         = 'Обновить';
+$_lang['reviews.review_remove']                         = 'Удалить';
+$_lang['reviews.review_remove_confirm']                 = 'Вы уверены, что хотите удалить этот отзыв? Он больше не будет включен в статистику.';
+$_lang['reviews.review_activate']                       = 'Активировать';
+$_lang['reviews.review_activate_confirm']               = 'Вы уверены, что хотите активировать этот отзыв? Он больше не будет включен в статистику.';
+$_lang['reviews.review_deactivate']                     = 'Дезактивировать';
+$_lang['reviews.review_deactivate_confirm']             = 'Вы уверены, что хотите деактивировать этот отзыв? Он больше не будет включен в статистику.';
+
+$_lang['reviews.label_resource']                        = 'Ресурс';
+$_lang['reviews.label_resource_desc']                   = 'Ресурс отзыва.';
+$_lang['reviews.label_name']                            = 'Имя';
+$_lang['reviews.label_name_desc']                       = 'Имя рецензента.';
+$_lang['reviews.label_email']                           = 'Адрес электронной почты';
+$_lang['reviews.label_email_desc']                      = 'Адрес электронной почты рецензента.';
+$_lang['reviews.label_rating']                          = 'Рейтинг';
+$_lang['reviews.label_rating_desc']                     = 'Рейтинг отзыва.';
+$_lang['reviews.label_content']                         = 'Отзыв';
+$_lang['reviews.label_content_desc']                    = 'Обзор.';
+$_lang['reviews.label_active']                          = 'Активный';
+$_lang['reviews.label_active_desc']                     = '';
+
+$_lang['reviews.filter_resource']                       = 'Фильтр по ресурсу';
+$_lang['reviews.rating_0']                              = 'звезд';
+$_lang['reviews.rating_1']                              = 'звезда';
+$_lang['reviews.rating_2']                              = 'звезды';
+$_lang['reviews.rating_3']                              = 'звезды';
+$_lang['reviews.rating_4']                              = 'звезды';
+$_lang['reviews.rating_5']                              = 'звезд';
+$_lang['reviews.title']                                 = 'Отзывов ([[+total]] всего)';
+$_lang['reviews.review_title']                          = 'Отзыв [[+idx]] из [[+total]] от';
+$_lang['reviews.no_reviews']                            = 'Нет отзывов об этом ресурсе.';
